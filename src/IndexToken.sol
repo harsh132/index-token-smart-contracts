65b3001b3bff9446bb7d2f824102c99dc3496f71
Harsh Prakash <prakashharsh32@gmail.com>
2024-09-28 17:37:10 +0530

    added oz

--- src/IndexToken.sol
+++ src/IndexToken.sol
@@ -1,13 +1,32 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.27;
 
-contract IndexToken {
-    uint256 private totalShares;
-    mapping(address=>uint) private shares;
+import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20PermitUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 
-    function deposit() public payable {
-
+contract IndexToken is Initializable, ERC20Upgradeable, OwnableUpgradeable, ERC20PermitUpgradeable {
+    constructor() {
+        _disableInitializers();
     }
 
+    function initialize(address initialOwner) initializer public {
+        __ERC20_init("IndexETH", "IETH");
+        __Ownable_init(initialOwner);
+        __ERC20Permit_init("IndexETH");
+    }
 
+    function deposit() public payable {
+        uint256 amount = msg.value * totalSupply() / address(this).balance;
+        _mint(msg.sender, amount);
+    }
+
+    function withdraw(uint256 amount) public {
+        require(balanceOf(msg.sender) >= amount, "Insufficient balance");
+        _burn(msg.sender, amount);
+
+        uint256 ethAmount = amount * address(this).balance / totalSupply();
+        payable(msg.sender).transfer(ethAmount);
+    }
 }
