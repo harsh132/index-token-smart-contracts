7020a26f197484f0e9729218d46529174e9d16f9
Harsh Prakash <prakashharsh32@gmail.com>
2024-09-28 07:21:37 +0530

    created IndexToken

---
+++ src/IndexToken.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.27;
+
+contract IndexToken {
+    uint256 private totalShares;
+    mapping(address=>uint) private shares;
+
+    function deposit() public payable {
+
+    }
+
+
+}
